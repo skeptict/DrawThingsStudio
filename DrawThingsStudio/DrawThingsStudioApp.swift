d7bd4e59d023fe68a115327ef557170c6c997ac2
skeptict <skeptict@users.noreply.github.com>
2026-01-14 17:12:54 -0500

    Add Xcode project

---
+++ DrawThingsStudio/DrawThingsStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawThingsStudioApp.swift
+//  DrawThingsStudio
+//
+//  Created by skeptict on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct DrawThingsStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
